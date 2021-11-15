4a106131f12b760d0073500bb0eda05bfa378778
christian aranda <kristian.aranda@gmail.com>
2021-11-15 15:25:38 +0100

    add index page

--- PointingPoker.Server/Program.cs
+++ PointingPoker.Server/Program.cs
@@ -1,7 +1,20 @@
-WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+﻿using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddBlazorise(options =>
+{
+    options.ChangeTextOnKeyPress = true;
+    options.DelayTextOnKeyPress = true;
+    options.DelayTextOnKeyPressInterval = 1000;
+})
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 
 WebApplication? app = builder.Build();
 
