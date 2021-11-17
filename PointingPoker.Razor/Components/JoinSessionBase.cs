edf6b6b202f7b4742076d0871c49b78bc8e3f362
christian aranda <kristian.aranda@gmail.com>
2021-11-17 21:38:28 +0100

    refactor

--- PointingPoker.Razor/Components/JoinSessionBase.cs
+++ PointingPoker.Razor/Components/JoinSessionBase.cs
@@ -10,4 +10,14 @@ public class JoinSessionBase : ComponentBase
     protected string PlayerName { get; set; } = string.Empty;
 
     protected Validations Validations { get; set; } = default!;
+
+    protected async Task OnJoinClickedAsync()
+    {
+        var isValid = this.Validations.ValidateAll();
+        if (!isValid)
+        {
+            return;
+        }
+
+    }
 }
