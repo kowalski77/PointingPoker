1697e2f553e5fa993068f4ea353410500704c0f5
christian aranda <kristian.aranda@gmail.com>
2021-11-20 17:12:07 +0100

    clean up

--- PointingPoker.Razor/ViewModels/SessionWithPlayersViewModel.cs
+++ PointingPoker.Razor/ViewModels/SessionWithPlayersViewModel.cs
@@ -1,5 +1,3 @@
 ﻿namespace PointingPoker.Razor.ViewModels;
 
 public record SessionWithPlayersViewModel(Guid Id, int SessionId, IEnumerable<PlayerViewModel> Players, IEnumerable<PointsViewModel> PointsAvailable);
-
-public record PlayerViewModel(Guid Id, string Name, DateTime TimeJoined, int? Points, bool IsObserver);
