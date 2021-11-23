1a05556c4c60e87b7cb4f0225377cd2632baa5b0
christian aranda <kristian.aranda@gmail.com>
2021-11-23 20:20:19 +0100

    evolve 2

--- PointingPoker.Razor/Components/ScoreBase.cs
+++ PointingPoker.Razor/Components/ScoreBase.cs
@@ -8,11 +8,11 @@ namespace PointingPoker.Razor.Components;
 
 public class ScoreBase : ComponentBase
 {
-    protected ICollection<ScoreViewModel> ScoreViewModels { get; } = new List<ScoreViewModel>();
+    protected ICollection<ScoreViewModel> ScoreViewModels { get; private set; } = new List<ScoreViewModel>();
 
     [Inject] private IGameConnectionHub GameConnectionHub { get; set; } = default!;
-    
-    [Inject] private IScoreCache ScoreCache { get; set; } = default!;   
+
+    [Inject] private IScoreCache ScoreCache { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
@@ -20,38 +20,31 @@ public class ScoreBase : ComponentBase
         this.GameConnectionHub.OnVoteReceived(this.ReceiveVote);
         await this.GameConnectionHub.StartAsync().ConfigureAwait(false);
     }
-    
+
     private void ReceiveNewPlayer(PlayerViewModel player)
     {
         var sessionId = player.SessionId.ToString(CultureInfo.InvariantCulture);
 
-        var cachedScores = this.ScoreCache.Get(sessionId)?.ToList() ?? new List<ScoreViewModel>();
-        if(cachedScores.Any(x=>x.PlayerId == player.Id))
-        {
-            return;
-        }
+        var newlyScore = (ScoreViewModel)player;
 
-        cachedScores.Add((ScoreViewModel)player);
+        var cachedScoreViewModels = this.ScoreCache.Get(sessionId)?.ToList() ?? new List<ScoreViewModel>();
+        cachedScoreViewModels.Add(newlyScore);
+        this.ScoreCache.Update(sessionId, cachedScoreViewModels);
 
-        foreach (var cachedScore in cachedScores)
-        {
-            this.ScoreViewModels.Add(cachedScore);
-            this.StateHasChanged();
-        }
-
-        this.ScoreCache.Update(sessionId, cachedScores);
+        this.ScoreViewModels = new List<ScoreViewModel>(cachedScoreViewModels);
+        this.StateHasChanged();
     }
 
     private void ReceiveVote(PlayerVoteViewModel pointsViewModel)
     {
         var score = this.ScoreViewModels.FirstOrDefault(x => x.PlayerId == pointsViewModel.PlayerId);
-        if(score is null)
+        if (score is null)
         {
             return;
         }
 
         score.Points = pointsViewModel.Points.ToString(CultureInfo.InvariantCulture);
-        
+
         this.StateHasChanged();
     }
 }
